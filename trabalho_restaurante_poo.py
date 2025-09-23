41eee17776bddffc67465932c6645552ea51a530
HenriqueRMachado <henrique.machado25@univille.br>
2025-09-23 20:01:48 -0300

    Add files via upload

---
+++ trabalho_restaurante_poo.py
@@ -0,0 +1,43 @@
+def restaurante (): # funcao para manter o escopo do codigo, o que esta fora da funcao nao afeta o que esta dentro
+    
+    class funcionarios_restaurante:
+        def __init__(self):
+            pass
+
+    class clientes_restaurantes:
+        def __init__(self):
+            pass
+
+    class fornecedor_restaurante:
+        def __init__(self):
+            pass
+
+
+
+    class ingredientes:
+        def __init__(self, tomate):
+            pass
+
+    class pratos(ingredientes):
+        def __init__(self, tomate):
+            super().__init__(tomate)
+
+
+
+
+    class pedidos:
+        def __init__(self):
+            pass
+
+        def fazer_pedido(self):
+            print()
+
+        def devolver_pedido(self):
+            print()
+
+        def chamar_garcom(self):
+            print()
+
+    
+restaurante()    
+
